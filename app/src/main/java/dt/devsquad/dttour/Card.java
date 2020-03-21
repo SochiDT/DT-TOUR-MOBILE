5fd4c3f27c3ee8260ab66aca7eca125eebef5d49
kirsid <kirsid2000@mail.ru>
2020-03-21 17:24:07 +0300

    test

--- app/src/main/java/dt/devsquad/dttour/Card.java
+++ app/src/main/java/dt/devsquad/dttour/Card.java
@@ -7,12 +7,20 @@ public class Card {
     String textSecond;
     int value;
     int id;
-    JSONArray array;
+    private JSONArray array;
 
     Card(String textFirst, String textSecond,int value, int id){
         this.textFirst = textFirst;
         this.textSecond = textSecond;
         this.value = value;
         this.id = id;
+    }
+
+    public JSONArray getArray() {
+        return array;
+    }
+
+    public void setArray(JSONArray array) {
+        this.array = array;
     }
 }
